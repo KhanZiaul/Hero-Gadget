dc005f696a455c423d4bf2effa99c4d6fe3e6055
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-09 00:26:41 +0600

    iamge added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Home.css'
-
+import heroImg from '../../assets/hero.jpg'
 const Home = () => {
     return (
         <div>
             <h2 className='text-center text-6xl font-bold title-animation'>Welcome To HeroGadget</h2>
+            <p className='text-center my-5 font-semibold  text-gray-600'>Best E-commerce platform for buying high quality Smart Home <br />  Appliances at extremely affordable Price.</p>
+
+            <div className='w-[75%] mx-auto'>
+                <img src={heroImg} alt="" />
+            </div>
         </div>
     );
 };
