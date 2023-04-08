1cdd3cd87b007f6dbde28e1738e5e9eaa6caf353
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-09 01:04:43 +0600

    home component done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,11 +4,19 @@ import heroImg from '../../assets/hero.jpg'
 const Home = () => {
     return (
         <div>
-            <h2 className='text-center text-6xl font-bold title-animation'>Welcome To HeroGadget</h2>
-            <p className='text-center my-5 font-semibold  text-gray-600'>Best E-commerce platform for buying high quality Smart Home <br />  Appliances at extremely affordable Price.</p>
+            <div className='bg-gray-200 py-40'>
 
-            <div className='w-[75%] mx-auto'>
-                <img src={heroImg} alt="" />
+            <h2 className='text-center text-6xl font-bold title-animation'>Welcome To HeroGadget</h2>
+            <p className='text-center my-5 '>Best E-commerce platform for buying high quality Smart Home <br />  Appliances at extremely affordable Price.</p>
+
+            <div className='flex justify-center items-center gap-4'>
+                <button className='block bg-sky-200 px-8 py-4 rounded-full drop-shadow-md font-semibold hover:bg-sky-400'>Shop now</button>
+                <button className='block bg-white px-8 py-4 rounded-full drop-shadow-md font-semibold hover:bg-sky-400'>Learn More</button>
+            </div>
+            </div>
+
+            <div className='w-[85%] h-[40%] mx-auto -mt-28'>
+                <img className='rounded-xl' src={heroImg} alt="" />
             </div>
         </div>
     );
